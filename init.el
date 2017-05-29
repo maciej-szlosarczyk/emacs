4e3bdc6352de30e576edb62523cfd97ac1ed7fde
Maciej Szlosarczyk <maciej.szlosarczyk@inbank.ee>
2017-05-29 11:20:20 +0300

    Abstract init from .emacs file

---
+++ init.el
@@ -0,0 +1,13 @@
+;;; init.el --- Summary
+;;; Commentary:
+;;; Code:
+(load "~/.emacs.d/packages.elc")
+(load "~/.emacs.d/functions.elc")
+(load "~/.emacs.d/behaviour.elc")
+(load "~/.emacs.d/themes.elc")
+(load "~/.emacs.d/key-bindings.elc")
+(load "~/.emacs.d/spelling.elc")
+(load "~/.emacs.d/languages.elc")
+
+(provide 'init)
+;;; init.el ends here
