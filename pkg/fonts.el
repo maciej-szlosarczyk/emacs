3d7c1b2f0d1b439ac7fe8c24b13a45b9c75a0d0f
Maciej Szlosarczyk <maciej@sosek.net>
2021-06-08 08:29:11 +0300

    Update fonts and packages

--- pkg/fonts.el
+++ pkg/fonts.el
@@ -21,7 +21,9 @@
   (set-face-attribute 'mode-line-inactive nil :font
                       (format "%s %d" name (- size 1))))
 
-(defconst +custom-font "JetBrains Mono")
+;; (defconst +custom-font "Iosevka Term")
+;; (defconst +custom-font "JetBrains Mono")
+(defconst +custom-font "IBM Plex Mono")
 
 (defun set-font-to-screen ()
   "Automatically set font size to suit the monitor."
@@ -29,10 +31,13 @@
   ;; If display is set to emulate FullHD resultion or less, make the font
   ;; smaller.
   (cond ((eq (x-display-list) nil)) ()
+        ;; built-in screen
         ((>= 1050 (x-display-pixel-height)) (set-font +custom-font 14))
-        ((>= 1080 (x-display-pixel-height)) (set-font +custom-font 13))
+        ;; 4K screen
+        ((>= 1080 (x-display-pixel-height)) (set-font +custom-font 14))
         ((>= 1120 (x-display-pixel-height)) (set-font +custom-font 14))
         ((>= 1440 (x-display-pixel-height)) (set-font +custom-font 16))
+        ;; 4K screen on Windows
         ((>= 2160 (x-display-pixel-height)) (set-font +custom-font 20))
         (t (set-font +custom-font 16))))
 
