00fe42ad44679ff346e68050ec32e2a56596d9cc
Maciej Szlosarczyk <maciej@sosek.net>
2024-10-21 19:54:07 +0300

    Fix broken projectile config

--- lisp/icejam-projectile.el
+++ lisp/icejam-projectile.el
@@ -8,17 +8,19 @@
 (use-package projectile
   :straight t
   :requires (counsel counsel-projectile)
+  :ensure t
+  :init
+  (projectile-mode)
+  (remove-hook 'buffer-list-update-hook #'projectile-track-known-projects-find-file-hook)
   :config
-  (setq-default projectile-completion-system 'ivy                  ;; Use ivy as completion system
-                projectile-enable-caching t                        ;; Cache project files
+  (setq-default projectile-completion-system 'ivy ;; Use ivy as completion system
+                projectile-enable-caching t ;; Cache project files
                 projectile-file-exists-local-cache-expire (* 3 60) ;; keep cache for 3 minutes
-                projectile-sort-order (quote recently-active)      ;; Order by recently active projects
+                projectile-sort-order (quote recently-active) ;; Order by recently active projects
                 projectile-globally-ignored-directories
                 (append '("node_modules" ".svn" "_build" "tmp" "log") projectile-globally-ignored-directories)
                 ;; Add certain folders to ignored
                 ))
-
-(projectile-mode t)
 
 (provide 'icejam-projectile)
 ;;; icejam-projectile.el ends here
